b66dbe1c80f76c30ebee699fa7ded89fa512ad43
Darko Tacic <darko.tacic@outlook.com>
2020-04-17 19:38:35 +0200

    Added getters and setters for User class

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,1 +1,8 @@
-console.log('Test');
+import { User } from './models/User';
+
+const user = new User({ name: 'My name', age: 20 });
+console.log(user.get('name'));
+console.log(user.get('age'));
+user.set({ name: 'Darko' });
+console.log(user.get('name'));
+console.log(user.get('age'));
