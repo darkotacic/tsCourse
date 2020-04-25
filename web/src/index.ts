8be2fcc96710f32e006a4d73cb02e863a0068f8c
Darko Tacic <darko.tacic@outlook.com>
2020-04-25 15:05:34 +0200

    Implemented on and trigger methods

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,8 +1,17 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'My name', age: 20 });
-console.log(user.get('name'));
-console.log(user.get('age'));
-user.set({ name: 'Darko' });
-console.log(user.get('name'));
-console.log(user.get('age'));
+
+user.on('darko', () => {
+  console.log('HELLO WORLD123123 TAMCE TAMCE');
+});
+
+user.on('darko', () => {
+  console.log('HELLO WORLD1234 TAMARA');
+});
+
+user.on('darko', () => {
+  console.log('HELLO WORLD TAMARA TAMCI <3 TAMCE');
+});
+
+user.trigger('darko');
