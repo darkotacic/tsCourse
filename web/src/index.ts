ecabc8056c2182f9a37f2466aa5dfab8a6adcefe
Darko Tacic <darko.tacic@outlook.com>
2020-05-27 02:12:35 +0200

    Final commit. Impl collectionView,UserEdit,Form

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,4 +1,19 @@
-import { UserForm } from './views/UserForm';
+import { UserProps, User } from './models/User';
+import { Collection } from './models/Collection';
+import { UserList } from './views/UserList';
 
-const userForm = new UserForm(document.getElementById('root'));
-userForm.render();
+const users = new Collection(
+  'http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
+
+users.on('change', () => {
+  const root = document.getElementById('root');
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+users.fetch();
---
+++ web/src/views/UserList.ts
@@ -0,0 +1,9 @@
+import { UserShow } from './UserShow';
+import { UserProps, User } from './../models/User';
+import { CollectionView } from './CollectionView';
+
+export class UserList extends CollectionView<User, UserProps> {
+  renderItem(model: User, itemParent: Element): void {
+    new UserShow(itemParent, model).render();
+  }
+}
