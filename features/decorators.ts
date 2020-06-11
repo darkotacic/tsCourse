29144f6b23f459d7782d726c74a06af659375c01
Darko Tacic <darko.tacic@outlook.com>
2020-06-12 00:27:41 +0200

    Finished the basics of decorators.

--- features/decorators.ts
+++ features/decorators.ts
@@ -1,16 +1,46 @@
+@classDecorator
 class Boat {
+  @testDecorator
   color: string = 'red';
 
   get formattedColor(): string {
     return `This boats color is ${this.color}`;
   }
-  @testDecorator
-  pilot(): void {
-    console.log('swish');
+  @logError('Oops boat was sunk in ocean!')
+  pilot(
+    @parameterDecorator speed: string,
+    @parameterDecorator generateWake: boolean
+  ): void {
+    if (speed === 'fast') {
+      console.log('swish');
+    } else {
+      console.log('nothing');
+    }
   }
 }
 
-function testDecorator(target: any, key: string): void {
-  console.log('Target: ', target);
-  console.log('Key: ', key);
+function parameterDecorator(target: any, key: string, index: number) {
+  console.log(key, index);
+}
+
+function classDecorator(constructor: typeof Boat) {
+  console.log(constructor);
+}
+
+function testDecorator(target: any, key: string) {
+  console.log(target);
+  console.log(key);
+}
+
+function logError(errorMessage: string) {
+  return function (target: any, key: string, desc: PropertyDescriptor): void {
+    const method = desc.value;
+    desc.value = function () {
+      try {
+        method();
+      } catch (e) {
+        console.log(errorMessage);
+      }
+    };
+  };
 }
