396cf59318ea9f57a75614a94d55eb9481f4906f
Darko Tacic <darko.tacic@outlook.com>
2020-06-27 23:00:48 +0200

    Started refactoring project with dec and metadata

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,15 @@
-import express, { Request, Response } from 'express';
-import { router } from './routes/loginRoutes';
+import express from 'express';
+import { router as controllerRouter } from './controllers/decorators/controller';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
+import './controllers/LoginController';
+import { router } from './routes/loginRoutes';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['laskfjgf'] }));
+app.use(controllerRouter);
 app.use(router);
 
 app.listen(3000, () => {
