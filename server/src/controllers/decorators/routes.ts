a29fec724d810b1e635c546f8463cf33f859cb02
Darko Tacic <darko.tacic@outlook.com>
2020-06-30 01:01:45 +0200

    implemented use decorator and root controller

--- server/src/controllers/decorators/routes.ts
+++ server/src/controllers/decorators/routes.ts
@@ -1,11 +1,12 @@
 import { Methods } from './Methods';
+import { MetadataKeys } from './MetadataKeys';
 import 'reflect-metadata';
 
 function routeBinder(method: string) {
   return function (path: string) {
     return function (target: any, key: string, desc: PropertyDescriptor) {
-      Reflect.defineMetadata('path', path, target, key);
-      Reflect.defineMetadata('method', method, target, key);
+      Reflect.defineMetadata(MetadataKeys.path, path, target, key);
+      Reflect.defineMetadata(MetadataKeys.method, method, target, key);
     };
   };
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,14 +3,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
 import './controllers/LoginController';
-import { router } from './routes/loginRoutes';
+import './controllers/RootController';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['laskfjgf'] }));
 app.use(AppRouter.getInstance());
-app.use(router);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
